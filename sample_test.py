41bff87fdd20ea2983c08f5cb249f562acffee2d
speat2471 <91529957+speat2471@users.noreply.github.com>
2024-04-11 21:53:52 +0100

    Update sample_test.py

--- sample_test.py
+++ sample_test.py
@@ -8,7 +8,7 @@ def test_get_titles(base_url):
     data = response.json()
     assert "productNames" in data
     # Add more assertions to check the structure and content of the response
-    expected_names = ["Product 1", "Product 2", "Product 3"]  # Update with your expected product names
+    expected_names = ["eggs", "bread", "Coffee beans"]  # Update with your expected product names
     assert data["productNames"] == expected_names
 
 # Test for '/getProducts' endpoint
@@ -21,9 +21,9 @@ def test_get_products(base_url):
     assert len(data) == 3  # Assuming there are 3 products in the database
     # Define your expected data here
     expected_data = [
-        {"id": "1", "name": "Product 1", "cost": 10.99},
-        {"id": "2", "name": "Product 2", "cost": 20.99},
-        {"id": "3", "name": "Product 3", "cost": 30.99}
+        {"id": "", "name": "eggs", "cost": 2.99},
+        {"id": "", "name": "bread", "cost": 1.99},
+        {"id": "1", "name": "Coffee Beans", "cost": 7.99}
     ]
     assert data == expected_data
 
