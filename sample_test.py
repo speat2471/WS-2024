f076adff17d9c42ed0340d426d773c038e36ca24
speat2471 <91529957+speat2471@users.noreply.github.com>
2024-04-11 14:54:11 +0100

    Update sample_test.py

--- sample_test.py
+++ sample_test.py
@@ -14,19 +14,19 @@ def test_get_products(base_url):
     assert response.status_code == 200
     # Add assertions to check the content and structure of the response
 
-def test_insert_product(base_url):
-    url = f"{base_url}/insertProduct?api_key=letmein"
-    payload = {
-        "id": "123",
-        "name": "Test Product",
-        "cost": 10.99
-    }
-    response = requests.post(url, json=payload)
-    assert response.status_code == 201
+#def test_insert_product(base_url):
+    #url = f"{base_url}/insertProduct?api_key=letmein"
+    #payload = {
+      #  "id": "123",
+      #  "name": "Test Product",
+      #  "cost": 10.99
+  #  }
+  #  response = requests.post(url, json=payload)
+   # assert response.status_code == 201
     # Optionally, check if the product is actually inserted into the database
 
 if __name__ == "__main__":
     base_url = "http://127.0.0.1:5000"  # Update with your actual base URL
     test_get_titles(base_url)
     test_get_products(base_url)
-    test_insert_product(base_url)
+  #  test_insert_product(base_url)
